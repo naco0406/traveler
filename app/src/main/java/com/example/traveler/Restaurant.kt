3b37e174605fe64d07a93215e1630dee849c3ec0
naco0406 <young0406@kaist.ac.kr>
2024-01-08 12:55:42 +0900

    네이버 지도 검색

---
+++ app/src/main/java/com/example/traveler/Restaurant.kt
@@ -0,0 +1,12 @@
+package com.example.traveler
+data class Restaurant(
+    val title: String,
+    val link: String,
+    val category: String,
+    val description: String,
+    val telephone: String,
+    val address: String,
+    val roadAddress: String,
+    val mapx: Long,
+    val mapy: Long
+)
