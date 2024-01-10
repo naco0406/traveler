b5f6b0d3224c602646b8ac124298326f21274b00
naco0406 <young0406@kaist.ac.kr>
2024-01-10 16:06:28 +0900

    Trip 불러오기, 번호 입력 자동화

--- app/src/main/java/com/example/traveler/ViewPager2Adapter.kt
+++ app/src/main/java/com/example/traveler/ViewPager2Adapter.kt
@@ -8,11 +8,14 @@ import androidx.viewpager2.adapter.FragmentStateAdapter
 
 class ViewPager2Adapter(fragmentActivity: FragmentActivity) : FragmentStateAdapter(fragmentActivity) {
 
+    private var itemCount = 3
 
-    private val fragmentList: MutableList<Fragment> = mutableListOf(FirstFragment(),SecondFragment(),ThirdFragment(),MapFragment())
-    private val itemCount = fragmentList.size
     override fun getItemCount(): Int {
-        return 4
+        return itemCount
+    }
+    fun updateItemCount(newCount: Int) {
+        itemCount = newCount
+        notifyDataSetChanged() // 아이템 개수 변경을 알림
     }
 
     override fun createFragment(position: Int): Fragment {
@@ -26,9 +29,5 @@ class ViewPager2Adapter(fragmentActivity: FragmentActivity) : FragmentStateAdapt
 //            6 -> SearchPlaceFragment()
             else -> throw IndexOutOfBoundsException("Invalid fragment position")
         }
-    }
-    fun addFragment(fragment: Fragment) {
-        fragmentList.add(fragment)
-        notifyDataSetChanged()
     }
 }
